06fcf368dba7c16f0056e4667ac57d1f274ef7e8
dan-prince <dan.prince@geckorobotics.com>
2022-01-28 23:17:13 -0500

    Add organisms of different types

---
+++ Assets/Scripts/OrganismModel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum OrganismType
+{
+    Green,
+    Red,
+    Blue
+}
+
+public class OrganismModel : MonoBehaviour
+{
+    public OrganismType type;
+
+    public bool CanEatType(OrganismType otherType)
+    {
+        return (type == OrganismType.Green && otherType == OrganismType.Blue)
+             || (type == OrganismType.Red && otherType == OrganismType.Green)
+             || (type == OrganismType.Blue && otherType == OrganismType.Green);
+    }
+
+}
