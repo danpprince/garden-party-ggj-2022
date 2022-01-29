77244c5bd0d4cb253654d69f838b49664f8eca56
dan-prince <dan.prince@geckorobotics.com>
2022-01-28 23:02:44 -0500

    Add organism reproduction

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,15 +9,22 @@ public class GameManager : MonoBehaviour
     public int numRows = 10, numCols = 10, numInitialOrganisms = 5;
 
     public GameObject tilePrefab;
-    public GameObject organism;
+    public GameObject organismPrefab;
+
+    public float simulationUpdatePeriodSec = 1.0f;
+
+    public float reproductionProbability = 0.1f;
 
     private List<List<GameObject>> generatedTiles;
     private List<GameObject> organisms;
+    private float lastSimulationUpdateSec;
 
     void Start()
     {
         InitializeTiles();
         InitializeOrganisms();
+
+        lastSimulationUpdateSec = 0;
     }
 
     void InitializeTiles()
@@ -49,13 +56,41 @@ public class GameManager : MonoBehaviour
 
             Vector3 organismPosition = new Vector3(rowIndex, 0.5f, colIndex);
             Quaternion defaultRotation = new Quaternion();
-            organisms.Add(Instantiate(organism, organismPosition, defaultRotation));
+            organisms.Add(Instantiate(organismPrefab, organismPosition, defaultRotation));
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool isTimeToUpdateSimulation = Time.time - lastSimulationUpdateSec > simulationUpdatePeriodSec;
+        if (isTimeToUpdateSimulation)
+        {
+            // Make a copy of the organism list so we can mutate the original while looping
+            List<GameObject> organismCopy = new List<GameObject>(organisms);
+            foreach (GameObject org in organismCopy)
+            {
+                bool isReproducing = Random.value < reproductionProbability;
+                if (isReproducing)
+                {
+                    Vector3 parentPosition = org.transform.position;
+
+                    Vector3 childPosition = parentPosition;
+                    childPosition.x += Random.Range(-1, 2);
+                    childPosition.z += Random.Range(-1, 2);
+
+                    bool isWithinWorldBounds =
+                        0 <= childPosition.x && childPosition.x < numRows
+                        && 0 <= childPosition.z && childPosition.z < numCols;
+                    if (!isWithinWorldBounds)
+                    {
+                        continue;
+                    }
+
+                    Quaternion defaultRotation = new Quaternion();
+                    organisms.Add(Instantiate(organismPrefab, childPosition, defaultRotation));
+                }
+            }
+        }
     }
 }
