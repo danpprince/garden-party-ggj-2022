e5489736a2b3c36f9fb1fea6faa38f8de6fb9674
Colin Farrell <colinfarrell@protonmail.com>
2022-01-29 23:13:13 -0500

    merge level-reset-button branch and finalized prune branch in prep for main PR

--- Assets/Scripts/PruneOnPress.cs
+++ Assets/Scripts/PruneOnPress.cs
@@ -8,36 +8,13 @@ public class PruneOnPress : MonoBehaviour
     [SerializeField] KeyCode keyPruneRed;
     [SerializeField] KeyCode keyPruneGreen;
     [SerializeField] GameManager gameManager;
-<<<<<<< HEAD
-    
-    private List<GameObject> orgsToPrune;
-=======
     [SerializeField] float popPercentToPrune;
-<<<<<<< HEAD
-    
-    private List<GameObject> orgsToPrune;
-    private List<int> rowIndexPrune;
-    private List<int> colIndexPrune;
->>>>>>> cf.level-reset-button
-
-    [SerializeField] public float pruneCooldownPeriodSec = 3.0f;
-=======
     [SerializeField] public float pruneCooldownPeriodSec = 1.0f;
->>>>>>> cf.level-reset-button
     private float lastPruneSec;
     private bool isPruneTypeSelected = false;
 
     void Start()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        orgsToPrune = new List<GameObject>();
-        rowIndexPrune = new List<int>();
-        colIndexPrune = new List<int>();
->>>>>>> cf.level-reset-button
-=======
->>>>>>> cf.level-reset-button
         lastPruneSec = 0;
     }
 
@@ -64,44 +41,11 @@ public class PruneOnPress : MonoBehaviour
         }
         float currentTimeSec = Time.time;
         bool isTimeToPrune = currentTimeSec - lastPruneSec > pruneCooldownPeriodSec;
-<<<<<<< HEAD
-        if (isTimeToPrune)
-        {
-            lastPruneSec = currentTimeSec;
-            OrganismType pruneOrganismType = new OrganismType();
-            //Identify plant type to prune
-            if (Input.GetKey(keyPruneBlue))
-            {
-                pruneOrganismType = OrganismType.Blue;
-<<<<<<< HEAD
-            } else if (Input.GetKey(keyPruneGreen))
-            {
-                pruneOrganismType = OrganismType.Green;
-            } else if (Input.GetKey(keyPruneRed))
-            {
-                pruneOrganismType = OrganismType.Red;
-            }
-
-=======
-                Debug.Log("Prune Blue Trees");
-            } else if (Input.GetKey(keyPruneGreen))
-            {
-                pruneOrganismType = OrganismType.Green;
-                Debug.Log("Prune Green Vines");
-            } else if (Input.GetKey(keyPruneRed))
-            {
-                pruneOrganismType = OrganismType.Red;
-                Debug.Log("Prune Red Weeds");
-            }
-
-=======
         if (isTimeToPrune && isPruneTypeSelected)
         {            
             isPruneTypeSelected = false;
             lastPruneSec = currentTimeSec;     
->>>>>>> cf.level-reset-button
             //crawl through grid looking for organisms, check the type for pruning
->>>>>>> cf.level-reset-button
             for (int rowIndex = 0; rowIndex < gameManager.numRows; rowIndex++)
             {
                 for (int colIndex = 0; colIndex < gameManager.numCols; colIndex++)
@@ -111,15 +55,6 @@ public class PruneOnPress : MonoBehaviour
                     {
                         continue;
                     }
-<<<<<<< HEAD
-
-                }
-            }
-
-            
-
-            //Prune %% of population
-=======
                     if (organism.GetComponent<OrganismModel>().type == pruneOrganismType)
                     {
                         bool isPruning = Random.value < popPercentToPrune;
@@ -132,29 +67,6 @@ public class PruneOnPress : MonoBehaviour
                     }
                 }
             }
-<<<<<<< HEAD
-
-            //Prune %% of population
-            int numToPrune = Mathf.RoundToInt(popPercentToPrune*orgsToPrune.Count);
-            for(int i = 0; i < numToPrune; i++)
-            {
-                int k = Random.Range(0, orgsToPrune.Count);
-                if (orgsToPrune[k] is null)
-                {
-                    i--;
-                    Debug.Log("Null encountered during pruning");
-                }
-                else
-                {
-                    Destroy(orgsToPrune[k]);
-                    gameManager.organismGrid[rowIndexPrune[k]][colIndexPrune[k]] = null;
-                    Debug.Log("Plant pruned successfully!");
-                }
-                
-            }
->>>>>>> cf.level-reset-button
-=======
->>>>>>> cf.level-reset-button
         }
     }
 }
